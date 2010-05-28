fa7c15166ab52cd746313d7c60c647c9486605c2
Guillaume Filion <gfk@ali.gfk-palace.org>
2010-05-28 14:21:33 -0400

    Added parallel querying with bgsend or libevent.

--- monitor/monitor.pl
+++ monitor/monitor.pl
@@ -11,6 +11,8 @@ use lib 'lib';
 use Monitor qw(read_config dbh);
 
 my $resolv = Net::DNS::Resolver->new();
+$resolv->tcp_timeout(2);
+$resolv->udp_timeout(2);
 
 my $dbh = dbh();
 
@@ -18,30 +20,40 @@ my $config = read_config();
 collect_query_counts();
 
 sub collect_query_counts {
-
     my $sth_insert = $dbh->prepare
         (q[insert into measurements
            (ip, measurement_time, queries, query_time)
            values (?,?,?,?)]
         );
 
-    for my $ip (keys %{$config->{servers}}) {
-        my $c = $config->{servers}->{$ip};
-        #print "checking $ip/$c->{name}\n";
-        $resolv->nameservers($ip);
-        my $time = time;
-        my $status_query = $resolv->query('status.pool.ntp.org', 'TXT');
-        my $elapsed = time - $time;
-        for my $rr ($status_query->answer) {
-            next unless $rr->type eq "TXT";
-            #print $rr->rdatastr, "\n";
-            my ($queries) = ($rr->rdatastr =~ m/q: (\d+)/)[0];
-            #print "Q: $queries\n";
-            my $ip24 = $c->{ip24};
-            $sth_insert->execute($ip24, int $time, $queries, $elapsed);
-        }
+		my %sockets; my $time = time;
+    for my $ns (keys %{$config->{servers}}) {
+    	$resolv->nameservers($ns);
+    	$sockets{$ns} = $resolv->bgsend('status.pool.ntp.org', 'TXT');
     }
-
+    
+    for my $i (1 .. 5) {
+    	for my $ns (sort keys %sockets) {
+    		my $socket = $sockets{$ns};
+    		if ($socket && $resolv->bgisready($socket)) {
+    			my $packet = $resolv->bgread($socket);
+    			my $elapsed = time - $time; # Not that precise...
+    			my ($txt) = $packet && grep { $_->type eq 'TXT' } $packet->answer;
+    			delete $sockets{$ns};
+    			
+          #print $txt->rdatastr, "\n";
+          my ($queries) = ($txt->rdatastr =~ m/q: (\d+)/)[0];
+          #print "$ns: Q:$queries E:$elapsed\n";
+          my $c = $config->{servers}->{$ns};
+          my $ip24 = $c->{ip24};
+          $sth_insert->execute($ip24, int $time, $queries, $elapsed);
+    		}
+      }
+      
+      last unless %sockets;
+ 
+      sleep 1;
+    }
 }
 
 1;
